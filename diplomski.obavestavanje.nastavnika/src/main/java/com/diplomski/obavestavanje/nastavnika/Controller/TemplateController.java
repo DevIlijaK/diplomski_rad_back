49b74b1783b106d0fd8642306de9e072dcc33e1b
Ilija Kosanin <ilija.kosanin@plusplusnt.rs>
2022-09-09 17:24:29 +0200

    security

---
+++ diplomski.obavestavanje.nastavnika/src/main/java/com/diplomski/obavestavanje/nastavnika/Controller/TemplateController.java
@@ -0,0 +1,19 @@
+package com.diplomski.obavestavanje.nastavnika.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/")
+@Controller
+public class TemplateController {
+
+    @GetMapping("login")
+    public String getLoginView(){
+        return "login";
+    }
+    @GetMapping("getCourses")
+    public String getCourses(){
+        return "courses";
+    }
+}
--- diplomski.obavestavanje.nastavnika/src/main/java/com/diplomski/obavestavanje/nastavnika/Security/ApplicationSecurityConfig.java
+++ diplomski.obavestavanje.nastavnika/src/main/java/com/diplomski/obavestavanje/nastavnika/Security/ApplicationSecurityConfig.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -12,59 +13,74 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 @EnableWebSecurity
 @AllArgsConstructor
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
-  @Autowired
-  private final PasswordEncoder passwordEncoder;
+    @Autowired
+    private final PasswordEncoder passwordEncoder;
 
-  @Override
-  protected void configure(HttpSecurity http) throws Exception {
-    http
-      .csrf().disable() // TODO: I will teach this in detail in the next section
-      .authorizeRequests()
-      .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
-      .antMatchers("/api/**").hasRole(ApplicationUserRole.STUDENT.name())
-//                .antMatchers(HttpMethod.DELETE, "/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
-//                .antMatchers(HttpMethod.POST, "/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
-//                .antMatchers(HttpMethod.PUT, "/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
-//                .antMatchers("/management/api/**").hasAnyRole(ADMIN.name(), ADMINTRAINEE.name())
-      .anyRequest()
-      .authenticated()
-      .and()
-      .httpBasic();
-  }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                    //                .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                    //                .and()
+                    .csrf().disable()
+                    .authorizeRequests()
+                    //                .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
+                    .antMatchers("/api/**").hasRole(ApplicationUserRole.STUDENT.name())
+                    .anyRequest()
+                    .authenticated()
+                    .and()
+                    .formLogin()
+                    .loginPage("/login").permitAll()
+                    .defaultSuccessUrl("/courses", true)
+                    .and()
+                    .rememberMe()
+                    .tokenValiditySeconds((int) TimeUnit.DAYS.toSeconds(21))
+                    .key("somethingverysecured")
+                    .and()
+                    .logout()
+                    .logoutUrl("/logout")
+                    .clearAuthentication(true)
+                    .invalidateHttpSession(true)
+                    .deleteCookies("JSESSIONID", "remember-me")
+                    .logoutSuccessUrl("/login");
+    }
 
-  @Override
-  @Bean
-  protected UserDetailsService userDetailsService() {
-    UserDetails annaSmithUser = User.builder()
-                                    .username("annasmith")
-                                    .password(passwordEncoder.encode("password"))
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        UserDetails annaSmithUser = User.builder()
+                                        .username("annasmith")
+                                        .password(passwordEncoder.encode("password"))
 //                .roles(STUDENT.name()) // ROLE_STUDENT
-                                    .authorities(ApplicationUserRole.STUDENT.getGrantedAuthorities())
+                                        .authorities(ApplicationUserRole.STUDENT.getGrantedAuthorities())
+                                        .build();
+
+        UserDetails lindaUser = User.builder()
+                                    .username("linda")
+                                    .password(passwordEncoder.encode("password123"))
+//                .roles(ADMIN.name()) // ROLE_ADMIN
+                                    .authorities(ApplicationUserRole.ADMIN.getGrantedAuthorities())
                                     .build();
 
-    UserDetails lindaUser = User.builder()
-                                .username("linda")
-                                .password(passwordEncoder.encode("password123"))
-//                .roles(ADMIN.name()) // ROLE_ADMIN
-                                .authorities(ApplicationUserRole.ADMIN.getGrantedAuthorities())
-                                .build();
-
-    UserDetails tomUser = User.builder()
-                              .username("tom")
-                              .password(passwordEncoder.encode("password123"))
+        UserDetails tomUser = User.builder()
+                                  .username("tom")
+                                  .password(passwordEncoder.encode("password123"))
 //                .roles(ADMINTRAINEE.name()) // ROLE_ADMINTRAINEE
-                              .authorities(ApplicationUserRole.ADMINTRAINEE.getGrantedAuthorities())
-                              .build();
-    return new InMemoryUserDetailsManager(
-      annaSmithUser,
-      lindaUser,
-      tomUser
-    );
-  }
+                                  .authorities(ApplicationUserRole.ADMINTRAINEE.getGrantedAuthorities())
+                                  .build();
+        return new InMemoryUserDetailsManager(
+                annaSmithUser,
+                lindaUser,
+                tomUser
+        );
+    }
 }

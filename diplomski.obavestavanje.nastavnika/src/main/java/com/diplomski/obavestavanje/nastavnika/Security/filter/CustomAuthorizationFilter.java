b5d0c58dc67e3ce99fd2212150c24abc51ff8560
Ilija Kosanin <ilija.kosanin@plusplusnt.rs>
2023-01-31 11:38:56 +0100

    januar

--- diplomski.obavestavanje.nastavnika/src/main/java/com/diplomski/obavestavanje/nastavnika/Security/filter/CustomAuthorizationFilter.java
+++ diplomski.obavestavanje.nastavnika/src/main/java/com/diplomski/obavestavanje/nastavnika/Security/filter/CustomAuthorizationFilter.java
@@ -36,7 +36,7 @@ public class CustomAuthorizationFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        if (request.getServletPath().equals("/api/login") || request.getServletPath().equals("/api/user/token/refresh")) {
+        if (request.getServletPath().equals("/api/user/login") || request.getServletPath().equals("/api/user/token/refresh")) {
             filterChain.doFilter(request, response);
         } else {
             String authorizationHeader = request.getHeader(AUTHORIZATION);
@@ -56,11 +56,11 @@ public class CustomAuthorizationFilter extends OncePerRequestFilter {
                             new UsernamePasswordAuthenticationToken(username, null, authorities);
                     SecurityContextHolder.getContext().setAuthentication(authenticationToken);
                     filterChain.doFilter(request, response);
+                    log.info("JWT verification successful for user: {}", username);
                 } catch (Exception exception) {
                     log.error("Error logging in: {}", exception.getMessage());
                     response.setHeader("error", exception.getMessage());
                     response.setStatus(FORBIDDEN.value());
-//                    response.sendError(FORBIDDEN.value());
                     Map<String, String> error = new HashMap<>();
                     error.put("error_message", exception.getMessage());
                     response.setContentType(APPLICATION_JSON_VALUE);

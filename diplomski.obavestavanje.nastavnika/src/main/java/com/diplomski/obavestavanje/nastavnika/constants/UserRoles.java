fe1e546fb3bc16809c15e34daa998669081d3d1b
Ilija Kosanin <ilija.kosanin@plusplusnt.rs>
2023-06-12 15:54:07 +0200

    komentar

---
+++ diplomski.obavestavanje.nastavnika/src/main/java/com/diplomski/obavestavanje/nastavnika/constants/UserRoles.java
@@ -0,0 +1,7 @@
+package com.diplomski.obavestavanje.nastavnika.constants;
+
+public enum UserRoles {
+    ROLE_ADMIN,
+    ROLE_STUDENT,
+    ROLE_PROFESSOR
+}
--- diplomski.obavestavanje.nastavnika/src/main/java/com/diplomski/obavestavanje/nastavnika/controller/AppUserController.java
+++ diplomski.obavestavanje.nastavnika/src/main/java/com/diplomski/obavestavanje/nastavnika/controller/AppUserController.java
@@ -4,6 +4,7 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.DecodedJWT;
+import com.diplomski.obavestavanje.nastavnika.constants.UserRoles;
 import com.diplomski.obavestavanje.nastavnika.model.ApplicationUser.AppUser;
 import com.diplomski.obavestavanje.nastavnika.model.ApplicationUser.Role;
 import com.diplomski.obavestavanje.nastavnika.service.AppUserService;
@@ -17,6 +18,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -39,30 +41,28 @@ public class AppUserController {
     private final AppUserService appUserService;
     private final JwtSecretKey jwtSecretKey;
 
-    /**
-     * Ova ruta mora da se zastiti da samoa dmin moze da joj pristupi
-     *
-     * @return
-     */
     @PostMapping("/get-users")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity<AppUsersSearchResponse> getUsers(@RequestBody GetAppUsersRequest getAppUsersRequest) {
-        log.info("nesto" + getAppUsersRequest);
         return ResponseEntity.ok().body(appUserService.getUsers(getAppUsersRequest));
     }
 
     @PostMapping("/save")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity<String> saveUsers(@RequestBody AppUser appUser) {
         URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/user/save").toUriString());
         return ResponseEntity.created(uri).body(appUserService.saveUser(appUser));
     }
 
     @PostMapping("/role/save")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity<Role> saveRole(@RequestBody Role role) {
         URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/user/role/save").toUriString());
         return ResponseEntity.created(uri).body(appUserService.saveRole(role));
     }
 
     @PostMapping("/role/add-to-user")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity<?> addRoleToUser(@RequestBody RoleToUserForm form) {
         appUserService.addRoleToUser(form.getUsername(), form.getRoleName());
         return ResponseEntity.ok().build();
@@ -125,11 +125,13 @@ public class AppUserController {
     }
 
     @GetMapping("get-roles")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity<List<Role>> getAppUserRoles() {
         return ResponseEntity.ok().body(appUserService.getAppUserRoles());
     }
 
     @PutMapping("/update-user")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity<Map<String, String>> updateUser(@RequestBody EditAppUserRequest editAppUserRequest) {
         Map<String, String> response = new HashMap<>();
         response.put("message", appUserService.updateAppUser(editAppUserRequest));
